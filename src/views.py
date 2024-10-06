cc778554812b2a9b10d234e537e0b415ac065742
Евгения Крюгер <Evgenia.Kryuger@yandex.ru>
2024-10-07 00:36:05 +0300

    Написаны функции

---
+++ src/views.py
@@ -0,0 +1,18 @@
+import json
+
+
+def form_json_response(
+        greeting,
+        card_info,
+        top_transactions,
+        currency_rates,
+        stock_prices):
+    """Формирует JSON-ответ"""
+    response = {
+        "greeting": greeting,
+        "cards": card_info,
+        "top_transactions": top_transactions,
+        "currency_rates": currency_rates,
+        "stock_prices": stock_prices,
+    }
+    return json.dumps(response, ensure_ascii=False, indent=4)
